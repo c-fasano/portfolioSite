b8e6722545fcd4a8a1e4f3b12844662eedcee226
Cole-Fasano <cole.fasano@outlook.com>
2022-01-10 09:57:25 -0600

    write styled components for about page

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,37 +4,59 @@ import self from "../images/Picture1.png";
 
 const AboutSection = () => {
   return (
-    <div>
+    <StyledAbout>
       <StyledDescription>
         <div className="title">
-          <div className="hidden">
+          <Hide>
             <h2>Hello There!</h2>
-          </div>
-          <div className="hidden">
-            <h2>I am Cole Fasano</h2>
-          </div>
-          <div className="hidden">
-            <h2>Welcome to my <span>Portfolio</span></h2>
-          </div>
+          </Hide>
+          <Hide>
+            <h2>I am <span>Cole Fasano</span></h2>
+          </Hide>
+          <Hide>
+            <h2>Welcome to my Portfolio</h2>
+          </Hide>
         </div>
+        <p>I am a full stack developer based in Peoria, Illinois. I am driven by delivering work that pleases both visually and functionally. </p>
         <button>Contact Me</button> 
       </StyledDescription>
-      <div className="image">
-        <img src={self} alt="Sitting down in chair Black and White" />
-      </div>
-    </div>
+      <StyledImage>
+        <img src={self} alt="Sitting down in chair Black and White" style={{width: "85%"}}/>
+      </StyledImage>
+    </StyledAbout>
   )
 }
 
 //Styled Components
 
-const StyledDescription = styled.div`
+const StyledAbout = styled.div`
   min-height: 90vh;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 5rem 10rem;
   color: white;
+`
+
+const StyledDescription = styled.div `
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`
+
+const StyledImage = styled.div `
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`
+const Hide = styled.div `
+  overflow: hidden;
 `
 
 export default AboutSection
--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -29,6 +29,36 @@ button {
   }
 }
 
+h2 {
+  font-weight: lighter;
+  font-size: 4rem;
+}
+
+h3{
+  color: white;
+}
+
+h4 {
+  font-weight: bold;
+  color: white;
+}
+
+a {
+  font-size: 1.1rem;
+}
+
+span {
+  font-weight: bold;
+  color: blue;
+}
+
+p {
+  padding: 3rem 0;
+  color: #ccc;
+  font-size: 1.4rem;
+  line-height: 150%;
+}
+
 `
 
 export default GlobalStyle
