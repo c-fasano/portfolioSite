159a4587e6768294102ca3612cc0fc4fa5076a0b
Cole-Fasano <cole.fasano@outlook.com>
2022-01-10 18:46:43 -0600

    write descriptions of projects

--- src/Pages/MyWork.jsx
+++ src/Pages/MyWork.jsx
@@ -10,11 +10,10 @@ import JQuery from "../images/jquery-4.svg";
 import JS from "../images/logo-javascript.svg";
 import Mongo from "../images/mongodb-icon-1.svg";
 import Node from "../images/nodejs-icon.svg";
+import Post from "../images/postgresql.svg";
 import Python from "../images/python-5.svg";
 import ReactImg from "../images/react-2.svg";
-import Redux from "../images/redux.svg"
 import SASS from "../images/sass-1.svg";
-import Stack from "../images/Stack.jpg";
 
 import { Logo, StyledAbout } from "../styles"
 
@@ -25,13 +24,13 @@ const MyWork = () => {
       <StyledProjects>
         <div className="project">
           <h2>Connect 4 Browser Game</h2>
-          <p>Here's a description of connect 4! :l</p>
+          <p>My first project - a browser verison of the classic Connect4 game.</p>
           <Logo className="Logo" src={JS} alt="JS Logo"/>
           <div className="divider"></div>
         </div>
         <div className="project">
           <h2>Brush Strokes</h2>
-          <p>Well this one was fun</p>
+          <p>This is a MEN stack application designed to be a platform where users could share their favorite artists and pieces. I also used Google Authentication in this application.</p>
           <Logo className="Logo" src={HTML} alt="HTML Logo"/>
           <Logo className="Logo" src={CSS} alt="CSS Logo"/>
           <Logo className="Logo" src={JS} alt="JS Logo"/>
@@ -41,18 +40,19 @@ const MyWork = () => {
         </div>
         <div className="project">
           <h2>Reflections</h2>
-          <p>Here's a description of connect 4! :l</p>
+          <p>This application is designed to be a place for individuals to jot down and store their memories. I created a custom logo for this project using ProCreate.</p>
           <Logo className="Logo" src={HTML} alt="HTML Logo"/>
           <Logo className="Logo" src={CSS} alt="CSS Logo"/>
           <Logo className="Logo" src={JS} alt="JS Logo"/>
           <Logo className="Logo" src={JQuery} alt="JQuery Logo"/>
           <Logo className="Logo" src={Python} alt="Python Logo"/>
           <Logo className="Logo" src={Django} alt="Django Logo"/>
+          <Logo className="Logo" src={Post} alt="PostgreSQL Logo"/>
           <div className="divider"></div>
         </div>
         <div className="project">
           <h2>Music Player</h2>
-          <p>Here's a description of connect 4! :l</p>
+          <p>This was the first React single page application that I created. This is also the first time I used SASS.</p>
           <Logo className="Logo" src={HTML} alt="HTML Logo"/>
           <Logo className="Logo" src={CSS} alt="CSS Logo"/>
           <Logo className="Logo" src={SASS} alt="SASS Logo"/>
@@ -62,7 +62,7 @@ const MyWork = () => {
         </div>
         <div className="project">
           <h2>Connect 4 Browser Game</h2>
-          <p>Here's a description of connect 4! :l</p>
+          <p>Full MERN stack application built with a team of myself and 3 others. This application is for freelanceers to utilize as a tool to keep their work, clients and themselves organized.</p>
           <Logo className="Logo" src={HTML} alt="HTML Logo"/>
           <Logo className="Logo" src={CSS} alt="CSS Logo"/>
           <Logo className="Logo" src={SASS} alt="SASS Logo"/>
@@ -81,6 +81,7 @@ const MyWork = () => {
 const StyledProjects = styled(StyledAbout)`
   display: block;
   h2 {
+    font-size: 2.5rem;
     padding-bottom: 1rem;
     font-weight: lighter;
   }
